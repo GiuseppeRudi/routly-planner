9586a7c2b559972bd5b957ecce17b0d80eaa08bc
GiuseppeRudi <giu200383.gr@gmail.com>
2026-07-14 16:46:21 +0200

    added optimizations for the configurations yaml
    Co-authored-by: eielpa <eielpa@users.noreply.github.com>

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -380,4 +380,7 @@ if __name__ == "__main__":
 # TODO il controllo se il path tra start e goal è percorribile deve anche tenere conto se il veicolo ha abbastanza benzina
 # TODO per arrivare al goal o se deve passare da una stazione di benzina per rifornirsi.
 
-# TODO 13. controllare un problema con route.edges in sumo_writer.py nella funzione write_rou_xml
+# SOLVED 13. controllare un problema con route.edges in sumo_writer.py nella funzione write_rou_xml
+
+# BUG Quando si effettua "--resume" su un experiment già runnato, con una configurazione dei nodi 
+# BUG differente rispetto al project.yaml globale, generate plan o plan to sumo falliscono
